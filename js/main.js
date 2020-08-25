c733db5dc911a802c7359983a7573ee56ae78e79
Tapri Andi <tapri.andi@aitindo.com>
2020-08-26 06:01:17 +0700

    update style section add message tooltips

--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
 $(window).ready( function() {
   $('.menu').click( function() {
     $('.box-lines').toggleClass('active')
+  });
+
+  // set width
+  var widthBg = $('.main-bg img').width();
+  $('.sections').css({width: widthBg + 'px'});
+  console.log(widthBg);
+
+  // set width on resize
+  $( window ).resize(function() {
+    widthBg = $('.main-bg img').width();
+    console.log(widthBg);
+
+    $('.sections').css({width: widthBg + 'px'});
+  });
+
+  $('#reg-eye').click(function() {
+    $('#reg-eye-msg').toggleClass('close')
+  })
+  $('#dry-eye').click(function() {
+    $('#dry-eye-msg').toggleClass('close')
+  })
+  $('#dry-product').click(function() {
+    $('#dry-product-msg').toggleClass('close')
   })
 
-  TweenLite.defaultEase = Linear.easeNone;
-  var titles = document.querySelectorAll(".sectionTitle");
-  var controller = new ScrollMagic.Controller();
-  var tl = new TimelineMax();
+  var titles = $(".sectionTitle"),
+      controller = new ScrollMagic.Controller(),
+      tl = new TimelineMax();
 
   // create timeline
   // this could also be created in a loop
   tl.to("#insto-slideContainer", 1, {xPercent: -20}, "label1");
-  tl.from(titles[1], 0.5, {opacity:0}, "label1+=0.5");
   tl.to("#insto-slideContainer", 1, {xPercent: -40}, "label2");
-  tl.from(titles[2], 0.5, {opacity:0}, "label2+=0.5");
   tl.to("#insto-slideContainer", 1, {xPercent: -60}, "label3");
-  tl.from(titles[3], 0.5, {opacity:0}, "label3+=0.5");
   tl.to("#insto-slideContainer", 1, {xPercent: -80}, "label4");
-  tl.from(titles[4], 0.5, {opacity:0}, "label4+=0.5");
-
 
   new ScrollMagic.Scene({
     triggerElement: "#insto-wrapper",
     triggerHook: "onLeave",
     duration: "1200%"
   })
-    .setPin("#insto-wrapper")
+    .setPin("#insto-slideContainer")
     .setTween(tl)
-    .addIndicators({
-      colorTrigger: "white",
-      colorStart: "white",
-      colorEnd: "white",
-    })
+    .addIndicators()
     .addTo(controller);
-
-
+  
+  
 })
+
+// $('.main-bg img').width() == $('.sections').width()
