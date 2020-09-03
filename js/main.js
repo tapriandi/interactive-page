54b1113fc820ae6e952490e041be8e9bcd99bbcf
Tapri Andi <tapri.andi@aitindo.com>
2020-09-03 15:10:26 +0700

    combibine horizontal scroll with preloader

--- js/main.js
+++ js/main.js
@@ -4,60 +4,57 @@ $(window).ready( function() {
   });
 
   // set width
-  var widthBg = $('.main-bg img').width();
-  $('.insto_wrapper').css({width: widthBg + 'px'});
-  // console.log(widthBg);
+  var mainBgWidth = $('.main-bg img').width();
+  $('.insto_wrapper').css({width: mainBgWidth + 'px'});
+  // console.log(mainBgWidth);
 
   // set width on resize
   $( window ).resize(function() {
-    widthBg = $('.main-bg img').width();
-    $('.insto_wrapper').css({width: widthBg + 'px'});
+    mainBgWidth = $('.main-bg img').width();
+    $('.insto_wrapper').css({width: mainBgWidth + 'px'});
   });
 
   // toggle btn message
   $('#reg-eye').click(function() {
     $('#reg-eye-msg').toggleClass('close')
-  })
+  });
   $('#dry-eye').click(function() {
     $('#dry-eye-msg').toggleClass('close')
-  })
+  });
   $('#dry-product').click(function() {
     $('#dry-product-msg').toggleClass('close')
-  })
+  });
   
   var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
   function instoPlay() {
-    $('#insto').css({
-      display: 'block', opacity: 1, zIndex: 1
-    })
-    const insto = document.querySelector('#insto')
-    const wrapper = document.querySelector('#wrapper')
-    const slides = insto.querySelectorAll('.insto_slide')
-    const amount = slides.length
-    const horizontal = new TimelineMax()
-    const controller = new ScrollMagic.Controller()
-    const controller2 = new ScrollMagic.Controller({
-      vertical: false,
-    })
+    var insto = document.querySelector('#insto'),
+      wrapper = document.querySelector('#wrapper'),
+      slides = insto.querySelectorAll('.insto_slide'),
+      amount = slides.length,
+      horizontal = new TimelineMax(),
+      controller = new ScrollMagic.Controller(),
+      controller2 = new ScrollMagic.Controller({
+        vertical: false
+      });
 
     horizontal.add([
       TweenMax.to(wrapper, 1, { x: `-${(100 / amount) * (amount - 1)}%` })
-    ])
+    ]);
 
     new ScrollMagic.Scene({
       triggerElement: insto,
       triggerHook: 'onLeave',
       // duration: '2500%'
-      duration: `${amount * 1000}%`
+      duration: `${amount * 2000}%`
     })
       .setPin(insto)
       .setTween(horizontal)
       .addTo(controller)
 
-    slides.forEach((item) => {
-      const pulse = item.querySelectorAll('.pulse-btn')
-      const tween = new TimelineMax()
+    slides.forEach(function(item) {
+      var pulse = item.querySelectorAll('.pulse-btn')
+      var tween = new TimelineMax()
       tween.to(pulse, 1, {
         transitionDelay: '1.5s',
         transform: 'scale(1)',
@@ -69,13 +66,12 @@ $(window).ready( function() {
         offset: -300,
         duration: '20%'
       })
-      .setTween(tween)
-      .addTo(controller2)
-      .addIndicators()
+        .setTween(tween)
+        .addTo(controller2)
+        .addIndicators()
+    });
 
-    })
-
-    // intialize scrollbar
+    // intialize smooth scrollbar
     var scroll = Scrollbar.init(
       document.querySelector("#insto")
     );
@@ -91,13 +87,13 @@ $(window).ready( function() {
       }
     });
   }
-  instoPlay()
-/**/
+  // instoPlay()
+
   // loader
   var dots = $('.dot'),
       loader = $('#loader'),
       tlLoader = new TimelineMax({
-        repeat: 3,
+        repeat: 1,
         onComplete: loadStart
       });
   tlLoader
@@ -115,32 +111,40 @@ $(window).ready( function() {
     );
   
   // after loading - start
-  function loadStart() {
-    $('#start').css({
-      display: 'block', opacity: 1, zIndex: 1
-    })
-  }
-/**/ 
-  // var boxImg = $('.box-img'),
-  //     insto = $('#insto'),
-  //     boxStart = $('.box-start'),
-  //     tlStart = new TimelineMax({
-  //       onComplete: instoPlay
-  //     });
-  // tlStart
-  //   to(boxImg, 1, {y: 300})
-    // .fromTo(boxStart, 1, {
-    //     autoAlpha: 1, scale: 1
-    //   }, {
-    //     autoAlpha: 0, ease: Power0.easeNone
-    //   },
-    //   1
-    // );
-    
+  var startContainer = $('#start'),
+      skyBgHeight = $('.sky-bg').height()
 
-    // $("#insto-start").on("click", function() {
-    //   tlStart.play();
-    // })
-    /**/
-    
+  function loadStart() {
+    new TimelineMax()
+      .to(startContainer, 1, {
+        display: 'block',
+        opacity: 1,
+        zIndex: 1
+      })
+  };
+
+  var instoStartBtn = $('#insto-start-btn')
+  function clickStart() {
+    new TimelineMax()
+      .to(start, 0.5, {
+        y: `-${skyBgHeight}px`
+      })
+  }
+
+  // insto play - after click button start
+  instoStartBtn.click(function() {
+    clickStart()
+    setTimeout(function() {
+      $('#insto').css({
+        display: 'block',
+        opacity: 1,
+        zIndex: 10,
+        transition: '0.8s'
+      });
+      $('.insto-start').css({
+        zIndex: '-1', display: 'none'
+      })
+      instoPlay()
+    }, 5000)
+  })
 })
