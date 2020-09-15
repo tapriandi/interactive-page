ca7f630141ef0c7fca0d35924048fa01902b8a05
Tapri Andi <tapri.andi@aitindo.com>
2020-09-15 14:47:07 +0700

    add lottie animation

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
-jQuery(window).ready( function() {
-  jQuery('.menu').click( function() {
+jQuery(window).ready(function () {
+  jQuery('.menu').click(function () {
     jQuery('.box-lines').toggleClass('active')
   });
 
   // set width
   var mainBgWidth = jQuery('.main-bg img').width();
-  jQuery('.insto_wrapper').css({width: mainBgWidth + 'px'});
+  jQuery('.insto_wrapper').css({
+    width: mainBgWidth + 'px'
+  });
   // console.log(mainBgWidth);
 
   // set width on resize
-  jQuery( window ).resize(function() {
+  jQuery(window).resize(function () {
     mainBgWidth = jQuery('.main-bg img').width();
-    jQuery('.insto_wrapper').css({width: mainBgWidth + 'px'});
+    jQuery('.insto_wrapper').css({
+      width: mainBgWidth + 'px'
+    });
   });
 
   // toggle btn message
-  jQuery('#reg-eye').click(function() {
+  jQuery('#reg-eye').click(function () {
     jQuery('#reg-eye-msg').toggleClass('close')
   });
-  jQuery('#dry-eye').click(function() {
+  jQuery('#dry-eye').click(function () {
     jQuery('#dry-eye-msg').toggleClass('close')
   });
-  jQuery('#dry-product').click(function() {
+  jQuery('#dry-product').click(function () {
     jQuery('#dry-product-msg').toggleClass('close')
   });
-  
+
   var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
   function instoPlay() {
@@ -39,20 +43,21 @@ jQuery(window).ready( function() {
       });
 
     horizontal.add([
-      TweenMax.to(wrapper, 1, { x: '-' +( 100 / amount) * (amount - 1) + '%' })
+      TweenMax.to(wrapper, 1, {
+        x: '-' + (100 / amount) * (amount - 1) + '%'
+      })
     ]);
 
     new ScrollMagic.Scene({
-      triggerElement: insto,
-      triggerHook: 'onLeave',
-      // duration: '2500%'
-      duration: amount * 2000 +'%'
-    })
+        triggerElement: insto,
+        triggerHook: 'onLeave',
+        duration: amount * 1200 + '%'
+      })
       .setPin(insto)
       .setTween(horizontal)
       .addTo(controller)
 
-    slides.forEach(function(item) {
+    slides.forEach(function (item) {
       var pulse = item.querySelectorAll('.pulse-btn')
       var tween = new TimelineMax()
       tween.to(pulse, 1, {
@@ -61,58 +66,73 @@ jQuery(window).ready( function() {
         animation: 'pulse 1.2s infinite cubic-bezier(0.3, 0, 0, 1)'
       }, 0)
       new ScrollMagic.Scene({
-        triggerElement: item,
-        triggerHook: 'onCenter',
-        offset: -300,
-        duration: '20%'
-      })
+          triggerElement: item,
+          triggerHook: 'onCenter',
+          offset: -300,
+          duration: '20%'
+        })
         .setTween(tween)
         .addTo(controller2)
-        .addIndicators()
+      // .addIndicators()
     });
 
     // intialize smooth scrollbar
     var scroll = Scrollbar.init(
       document.querySelector("#insto")
     );
+
     // listener smooth-scrollbar, update controller
-    scroll.addListener(function(status) {
+    scroll.addListener(function (status) {
       y = status.offset.y;
-      if(isChrome){ 
+      if (isChrome) {
         controller.update();
       } else {
-        scenes.forEach(function(scene){
-            scene.refresh();       
+        scenes.forEach(function (scene) {
+          scene.refresh();
         });
       }
     });
   }
   // instoPlay()
 
+
+  // lottie bike animation
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('insto-bike'),
+    path: '/path/insto-bike.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    name: "insto bike",
+  });
+
   // loader
   var dots = jQuery('.dot'),
-      loader = jQuery('#loader'),
-      tlLoader = new TimelineMax({
-        repeat: 1,
-        onComplete: loadStart
-      });
+    loader = jQuery('#loader'),
+    tlLoader = new TimelineMax({
+      repeat: 1,
+      onComplete: loadStart
+    });
   tlLoader
     .staggerFromTo(dots, 0.5, {
-        y: 0, autoAlpha: 0
-      }, {
-        y: 20, autoAlpha: 1, ease: Back.easeInOut
-      }, 0.1
-    )
+      y: 0,
+      autoAlpha: 0
+    }, {
+      y: 20,
+      autoAlpha: 1,
+      ease: Back.easeInOut
+    }, 0.1)
     .fromTo(loader, 0.3, {
-        autoAlpha: 1, scale: 1
-      }, {
-        autoAlpha: 0, ease: Power0.easeNone
-      }, 1
-    );
-  
+      autoAlpha: 1,
+      scale: 1
+    }, {
+      autoAlpha: 0,
+      ease: Power0.easeNone
+    }, 1);
+
   // after loading - start
   var startContainer = jQuery('#start'),
-      skyBgHeight = jQuery('.sky-bg').height()
+    skyBgHeight = jQuery('.sky-bg').height()
 
   function loadStart() {
     new TimelineMax()
@@ -124,6 +144,7 @@ jQuery(window).ready( function() {
   };
 
   var instoStartBtn = jQuery('#insto-start-btn')
+
   function clickStart() {
     new TimelineMax()
       .to(start, 0.5, {
@@ -132,17 +153,18 @@ jQuery(window).ready( function() {
   }
 
   // insto play - after click button start
-  instoStartBtn.click(function() {
+  instoStartBtn.click(function () {
     clickStart()
-    setTimeout(function() {
+    setTimeout(function () {
       jQuery('#insto').css({
         display: 'block',
         opacity: 1,
-        zIndex: 10,
+        zIndex: 1,
         transition: '0.8s'
       });
       jQuery('.insto-start').css({
-        zIndex: '-1', display: 'none'
+        zIndex: '-1',
+        display: 'none'
       })
       instoPlay()
     }, 5000)
