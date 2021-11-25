9c73900ccbc86e26bf5b5cc4864b5b0b589115b0
Tapri Andi <tapri.andi@aitindo.com>
2021-11-25 11:00:20 +0700

    init new design

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
 // play animation
-duplicateImage('bg-home', './assets/bg/rumah.png', 5)
-duplicateImage('bg-road', './assets/bg/jalan.png', 10)
+duplicateImage('bg-home', './assets/images/bg/rumah.png', 5)
+duplicateImage('bg-road', './assets/images/bg/jalan.png', 10)
 animTitle()
-setInterval(() => { document.getElementById('btn-start').classList.add('show') }, 3000); // 3000
-setInterval(() => { document.getElementById('character').classList.add('show') }, 12000); //12000
+setInterval(() => { document.getElementById('btn-start').classList.add('show') }, 3000);
+setInterval(() => { document.getElementById('character').classList.add('show') }, 10000);
 
 
 function start() {
   let splash = document.querySelector('.splash');
   splash.classList.add('hide');
-  
+
   document.querySelector('.bg-home').style.top = 0
   document.querySelector('.bg-shadow').style.top = 0
   document.querySelector('.bg-road').style.top = 0
   
+  
   initLottie()
   setInterval(() => {
     initSmoothScrollbar()
-  }, 13000); //13000
+    document.querySelector('.stories').style.display = 'flex'
+  }, 11000); //13000
 }
 
 
 
 
 // --------------------------------------------------------
-// variable.
+// variable
 var scrollbarInstance = null,
   direction = '',
   x = 0,
@@ -79,12 +81,10 @@ Scrollbar.use(HorizontalScrollPlugin);
 function initSmoothScrollbar() {
   scrollbarInstance = Scrollbar.init(
     document.querySelector(".scroll-wrapper"), {
-      damping: 0.04,
+      damping: 0.02,
     }
   );
   scrollbarInstance.addListener(listener);
-
-
 }
 
 // lottie
@@ -110,7 +110,8 @@ function listener({ offset }) {
   scrollPercent = (100 * scrollbarInstance.scrollLeft) / document.querySelector(".scene-wrapper").getBoundingClientRect().width;
   x = offset.x;
   parallax(document.querySelector('.bg-home > div'), x, -0.02, 1)
-  parallax(document.querySelector('.bg-road > div'), x, -0.4, 1)
+  parallax(document.querySelector('.bg-road > div'), x, -0.15, 1)
+  parallax(document.querySelector('.stories'), x, -0.15, 1)
 }
 
 // parallax
