360498c5d24c7ff646324456b106793f6879874e
Tapri Andi <tapri.andi@aitindo.com>
2021-12-21 14:03:48 +0700

    - responsive - remove-smoke effect - fixed scroll animation

--- js/main.js
+++ js/main.js
@@ -16,20 +16,26 @@ function start() {
   document.querySelector('.bg-shadow').style.top = 0
   document.querySelector('.bg-road').style.top = 0
   
-  initLottie()
+  
+  smokes('smoke-1', 10)
+  smokes('smoke-2', 40)
+  smokes('smoke-3', 80)
+  
   setTimeout(() => {
     document.querySelector('.stories').style.display = 'flex'
     initSmoothScrollbar()
   }, 10000); //13000
+  
+  
   setTimeout(() => {
+    initLottie()
     document.querySelector('.guide').classList.add('active')
     setInterval(() => {
       document.querySelector('.guide').classList.remove('active')
     }, 4000);
-  }, 11000)
-  smokes('smoke-1', 5)
-  smokes('smoke-2', 30)
-  smokes('smoke-3', 60)
+  }, 9000)
+
+
 }
 
 
@@ -42,7 +48,10 @@ var scrollbarInstance = null,
   x = 0,
   speed = 0.8,
   bike = null,
-  scrollPercent = 0
+  scrollPercent = 0,
+  widthContent = 0
+  widthContainer = 0
+  widthScreen = widthContent - widthContainer
 
 // opening title
 function animTitle() {
@@ -69,6 +78,42 @@ function animTitle() {
   }, 200);
 }
 
+// lottie
+function initLottie() {
+  bike = bodymovin.loadAnimation({
+    container: document.getElementById('character'),
+    path: './assets/insto-bike.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    name: "insto bike",
+  });
+}
+
+// direction listener and parallax
+function listener({ offset }) {
+  if (offset.x >= x) {
+    direction = "right";
+  } else {
+    direction = "left";
+  }
+
+  
+  scrollPercent = (100 * scrollbarInstance.scrollLeft) / document.querySelector(".scene-wrapper").getBoundingClientRect().width;
+  x = offset.x;
+  parallax(document.querySelector('.bg-home > div'), x, -0.06, 1)
+  parallax(document.querySelector('.bg-road > div'), x, -0.15, 1)
+  parallax(document.querySelector('.stories'), x, 0.9, 1)
+  
+  const currentScroll = scrollbarInstance.offset.x
+  // console.log(currentScroll, widthContent)
+  if (currentScroll > widthScreen) {
+    // scrollbarInstance.destroy()
+  }
+}
+
+
+
 // chage to horizontal
 class HorizontalScrollPlugin extends Scrollbar.ScrollbarPlugin {
   static pluginName = "horizontalScroll";
@@ -90,38 +135,13 @@ Scrollbar.use(HorizontalScrollPlugin);
 
 function initSmoothScrollbar() {
   scrollbarInstance = Scrollbar.init(
-    document.querySelector(".scroll-wrapper"), {
-      damping: 0.02,
-    }
+    document.querySelector(".scroll-wrapper"), 
+    { damping: 0.03 }
   );
   scrollbarInstance.addListener(listener);
-}
 
-// lottie
-function initLottie() {
-  bike = bodymovin.loadAnimation({
-    container: document.getElementById('character'),
-    path: './assets/insto-bike.json',
-    renderer: 'svg',
-    loop: true,
-    autoplay: false,
-    name: "insto bike",
-  });
-}
-
-// direction listener and parallax
-function listener({ offset }) {
-  if (offset.x >= x) {
-    direction = "right";
-  } else {
-    direction = "left";
-  }
-
-  scrollPercent = (100 * scrollbarInstance.scrollLeft) / document.querySelector(".scene-wrapper").getBoundingClientRect().width;
-  x = offset.x;
-  parallax(document.querySelector('.bg-home > div'), x, -0.05, 1)
-  parallax(document.querySelector('.bg-road > div'), x, -0.10, 1)
-  parallax(document.querySelector('.stories'), x, 0.9, 1)
+  widthContent = scrollbarInstance.getSize().content.width
+  widthContainer = scrollbarInstance.getSize().container.width
 }
 
 // parallax
@@ -168,6 +188,18 @@ function smokes(className, amount) {
   }
 }
 
+// btn remove smoke
+const btnRemoveArray = ['smoke-1', 'smoke-2', 'smoke-3']
+btnRemoveArray.forEach((e, index) => {
+  document.querySelector(`img.remove-${e}`).onclick = () => {
+    document.querySelector(`.scene-${index+1} .smokes`).classList.add('hide')
+    setTimeout(() => {
+      document.querySelector(`.scene-${index+1} .smokes`).classList.add('gone')
+    }, 3000);
+  }
+})
+
+
 // amaran
 function amaran() {
   document.querySelector('.pop-full').classList.add('active')
@@ -196,4 +228,3 @@ function closeAmaran() {
 
 //   // console.log(direction)
 // }
-
